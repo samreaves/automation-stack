506db58fb95bd9b7dc70a588c7af6cc021db3bf7
Sam <thesamreaves@gmail.com>
2018-03-31 11:02:14 -0400

    Forgot to hit save on service definition file

--- infrastructure/service-definition.tf
+++ infrastructure/service-definition.tf
@@ -1,7 +1,7 @@
 resource "aws_ecs_service" "test-ecs-service" {
   	name            = "automation-stack-ecs-service"
   	iam_role        = "${aws_iam_role.ecs-service-role.name}"
-  	cluster         = "${aws_ecs_cluster.test-ecs-cluster.id}"
+  	cluster         = "${aws_ecs_cluster.automation-stack-ecs-cluster.id}"
   	task_definition = "${aws_ecs_task_definition.wordpress.family}:${max("${aws_ecs_task_definition.wordpress.revision}", "${data.aws_ecs_task_definition.wordpress.revision}")}"
   	desired_count   = 2
 
